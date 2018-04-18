027d573165774bf8a4f11380ba99faecb790fc63
haoyuan336 <328387485@qq.com>
2018-04-19 00:15:38 +0800

    循环背景

--- js/game-world.js
+++ js/game-world.js
@@ -7,24 +7,35 @@ const GameWorld = function () {
     for (let i = 0 ; i < 2 ; i ++){
         let bg = new PIXI.Sprite.fromImage(defines.resPath + 'bg.png');
         that.node.addChild(bg);
-        bg.anchor = {
-            x: 0.5,
-            y: 0.5
-        };
         console.log('width = ' + bg.width);
-        bg.position = {
-            x: defines.designSize.width * 0.5 + bg.width * i,
-            y: defines.designSize.height * 0.5
-        };
         _bgList.push(bg);
+        bg.position = {
+            x: bg.width * i,
+            y: 0
+        }
     }
-    that.update = function () {
+
+
+
+
+
+
+    that.update = function (dt) {
         for (let i = 0 ; i < _bgList.length ; i ++){
             _bgList[i].position = {
                 x: _bgList[i].position.x - 1,
-                y: defines.designSize.height * 0.5
+                y: 0
+            };
+        }
+        for (let i = 0 ; i < _bgList.length ; i ++){
+            if ((_bgList[i].position.x + _bgList[i].width) <= 0){
+                _bgList[i].position = {
+                    x: _bgList[i].width,
+                    y: 0
+                }
             }
         }
+
     };
     return that;
 };
--- js/main.js
+++ js/main.js
@@ -1,31 +1,44 @@
 import * as PIXI from './libs/pixi.min'
 import defines from './defines'
 import GameWorld from './game-world'
+import resources from './resources'
 const Main = function(){
     console.log('main');
 
     let app = new PIXI.Application({width: defines.designSize.width, height: defines.designSize.height, view: canvas});
     let stage = new PIXI.Container();
 
-
-
-    let sp = new PIXI.Sprite.fromImage(defines.resPath + 'bg.png');
-    stage.addChild(sp);
-
-    const update = function (dt) {
-        console.log('dt  =' + dt);
-        sp.position = {
-            x: sp.position.x - 1 * dt,
-            y: sp.position.y
+    let _currentTime = new Date().getTime();
+    let _gameWorld = undefined;
+    const update = function () {
+        let currentTime = new Date().getTime();
+        let d = currentTime - _currentTime;
+        _currentTime = currentTime;
+        if (_gameWorld){
+            _gameWorld.update(d);
         }
     };
-    app.ticker.add(update);
+
 
     const referFrame = function () {
+        update();
         app.renderer.render(stage);
         requestAnimationFrame(referFrame, app.view);
     };
     referFrame();
+
+
+    for (let i in resources){
+        PIXI.loader.add(resources[i], resources[i]);
+    }
+    PIXI.loader.load((loader, res)=>{
+        window.resouces = res;
+        console.log('资源加载完毕');
+        _gameWorld = GameWorld();
+        stage.addChild(_gameWorld.node);
+    });
+
+
 
 };
 export default Main;
