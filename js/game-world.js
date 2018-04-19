3be08274991a7740f25b7de93c1cf34950285afd
楚浩远 <328387458@qq.com>
2018-04-19 20:29:09 +0800

    触摸监听

--- js/game-world.js
+++ js/game-world.js
@@ -14,13 +14,70 @@ const GameWorld = function () {
             y: 0
         }
     }
+    let _towersList = [];
 
 
+    //添加碰撞柱子
 
 
+    const addTower = function () {
+        let towerImage = ["pipeDown.png", "pipeUp.png"];
+        for (let i  = 0 ; i < 3 ; i ++){
+            let list = [];
+            let offsetY = Math.random() * (defines.designSize.height - 280) + 140;
+            for (let j = 0 ; j < 2 ; j ++){
+                let tower = new PIXI.Sprite.fromImage(defines.resPath + towerImage[j]);
+                that.node.addChild(tower);
+                let y = 0;
+                if (j ===0){
+                    y = offsetY - tower.height   - 50
+                }else {
+                    y = offsetY + 50
+                }
+                tower.position = {
+                    x: defines.designSize.width * 1.5 + 140 * i,
+                    y: y
+                };
+                list.push(tower);
+            }
+            _towersList.push(list);
+        }
+
+    };
+
+    //点击屏幕即可加载柱子
+    // that.node.on('');
+
+    const _touchBegin = function () {
+        console.log('touch began');
+    };
+    const _touchMoved = function () {
+        console.log('touch move');
+    };
+    const _touchCancel = function () {
+        console.log('touch cancel');
+    };
+    const _touchEnd = function () {
+        console.log('touch end');
+    };
+    const _onClick = function () {
+        console.log('click')
+    };
+    (()=> {
+        that.node.on("mousedown", _touchBegin)
+            .on("mousemove", _touchMoved)
+            .on("mouseout", _touchCancel)
+            .on("mouseup", _touchEnd)
+            .on("touchstart", _touchBegin)
+            .on("touchmove", _touchMoved)
+            .on("touchendoutside", _touchCancel)
+            .on("touchend", _touchEnd)
+            .on("click", _onClick)
+            .on("tap", _onClick);
+    })();
 
 
-    that.update = function (dt) {
+    that.update = function () {
         for (let i = 0 ; i < _bgList.length ; i ++){
             _bgList[i].position = {
                 x: _bgList[i].position.x - 1,
@@ -32,6 +89,35 @@ const GameWorld = function () {
                 _bgList[i].position = {
                     x: _bgList[i].width,
                     y: 0
+                }
+            }
+        }
+
+
+        for (let i = 0 ; i < _towersList.length ; i ++){
+            let list = _towersList[i];
+            for (let j = 0 ; j < list.length; j ++){
+                list[j].position = {
+                    x: list[j].position.x - 1,
+                    y: list[j].position.y
+                }
+            }
+        }
+        for (let i = 0 ; i < _towersList.length ; i ++){
+            let list = _towersList[i];
+            if ((list[0].position.x + list[0].width) < 0){
+                let offsetY = Math.random() * (defines.designSize.height - 280) + 140;
+                for (let j = 0 ; j < list.length ; j ++){
+                    let y = 0;
+                    if (j ===0){
+                        y = offsetY - list[j].height   - 50
+                    }else {
+                        y = offsetY + 50
+                    }
+                    list[j].position = {
+                        x: defines.designSize.width * 1.5 - list[j].width + 140,
+                        y: y
+                    }
                 }
             }
         }
